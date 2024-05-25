79e09732bbc05cc426854e4890da7ba81d87f0df
AHMED PASIC <ahmed.pasic@dccs.eu>
2024-05-26 00:02:14 +0200

    argocd: provision repo and application as well

--- deploy/argocd/deploy.sh
+++ deploy/argocd/deploy.sh
@@ -5,5 +5,3 @@ NAMESPACE="argocd"
 helm upgrade argocd ../../charts/argo-cd \
     --install --create-namespace --namespace "${NAMESPACE}" \
     -f values.yaml
-
-kubectl apply -f nodeport.yaml -n "${NAMESPACE}"
